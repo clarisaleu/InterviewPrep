aebef01a68c3f45e1c9c32fae2cfe3c4a3e40057
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-12-02 02:23:12 -0800

    up to queue

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,60 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Iterative Solution: O(n)
+function fib2(n) {
+  var res = [0, 1];
+  for (var i = 2; i <= n; i++) {
+    res.push(res[i - 1] + res[i - 2]);
+  }
+  return res[res.length - 1];
+}
+
+// Recursive Solution: exponential. for every increase of n, we are going to get a dramatic increase of function calls for fib.
+// you can improve it by avoiding extra function calls.
+// memoization: store the arguements of each function call along with the result.
+// if the function is called again with the same arguements, return the precomputed result,
+// rather than running the function again.
+function fib2(n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  return fib(n - 2) + fib(n - 1);
+}
+
+// Recursive Solution 2: exponential
+function fib3(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 2) + fib(n - 1);
+}
+
+// Recursive Solution 3: improved memoization process
+// Create cache object.
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    // defensive coding
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 2) + fib(n - 1);
+}
+
+fib = memoize(slowFib);
 
 module.exports = fib;
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,8 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) 
-    
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let count = 1;
+  let startColumn = 0;
+  let startRow = 0;
+  let endColumn = n - 1;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top Row
+    for (var i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = count;
+      count++;
+    }
+    startRow++;
+
+    //  Right Column
+    for (var i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = count;
+      count++;
+    }
+    endColumn--;
+
+    // Bottom Row
+    for (var i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = count;
+      count++;
+    }
+    endRow--;
+
+    // Start Column
+    for (var i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = count;
+      count++;
+    }
+
+    startColumn++;
+  }
+  return results;
 }
 
 module.exports = matrix;
